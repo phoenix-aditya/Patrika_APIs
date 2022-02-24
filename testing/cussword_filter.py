0df5d345b9be35519ddbeb91ef2d7a6b2191b986
phoenix-aditya <ranout.aditya@gmail.com>
2022-02-24 18:35:44 +0530

    update

--- testing/cussword_filter.py
+++ testing/cussword_filter.py
@@ -2,5 +2,5 @@ import requests
 import json
 
 str = "ass boobs"
-response = requests.get("https://patrika-api.herokuapp.com/"+"cussword-filter/"+str)
+response = requests.get("http://patrika-api.herokuapp.com/cussword-filter/{article: str}?article=there%20is%20boobs%20and%20ass%20here")
 print(response.json())
